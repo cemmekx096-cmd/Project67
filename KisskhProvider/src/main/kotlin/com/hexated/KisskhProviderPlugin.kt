9e87e414dea6a6f8e1048b6e325d361c12f2a0a8
cemmekx096-cmd <cemmekx096@gmail.com>
2026-07-19 00:47:47 +0700

    Delete KisskhProvider directory

--- KisskhProvider/src/main/kotlin/com/hexated/KisskhProviderPlugin.kt
+++
@@ -1,9 +0,0 @@
-package com.hexated
-
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.api.Provider
-
-@CloudstreamPlugin
-class KisskhProviderPlugin: Provider {
-    override val mainApi = KisskhProvider()
-}
